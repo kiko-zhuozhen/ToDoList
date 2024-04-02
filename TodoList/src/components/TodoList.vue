02b7c1c4fba26b1c17e9e3afaf64120ee07bef2c
kiko-zhuozhen <155774094+kiko-zhuozhen@users.noreply.github.com>
2024-04-02 14:36:29 -0500

    VueCommit

--- TodoList/src/components/TodoList.vue
+++ TodoList/src/components/TodoList.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="list">
-        <h1>To Do List</h1>
+        <h4>Search</h4>
+        <input v-model="searchText" type="text" placeholder="Search">
+        <h4 v-if="searchText">Search Results</h4>
+        <ul v-if="searchText">
+            <li v-for="(todo, index) in filterText" :key='index'>
+                {{ todo.text }}
+                <button @click="removeTodo(index)">Delete</button>
+            </li>
+        </ul>
+
+        <h4>To Do List</h4>
         <input v-model="newTodo" @keyup.enter="addTodo" type="text" placeholder="Add to do information">
         <button @click="addTodo">Add</button>
 
         <ul>
             <li v-for="(todo, index) in todos" :key='index'>
+                <input type="checkbox" v-model="checkTodo[index]" @change="removeChecked(index)">
                 {{ todo.text }}
                 <button @click="removeTodo(index)">Delete</button>
             </li>
@@ -14,14 +25,22 @@
 </template>
 
 <script lang="ts" setup name="Person">
-    import {ref} from 'vue'
+    import {ref, watchEffect} from 'vue'
 
+    let searchText=ref('')
     let newTodo = ref('')
     let todos = ref([] as { text:string }[])
+    let filterText = ref([] as { text: string }[])
+    let checkTodo = ref([] as boolean[] )
+
+    watchEffect(() => {
+        filterText.value = todos.value.filter(item => item.text.toLowerCase().includes(searchText.value.toLowerCase()))
+    })
 
     function addTodo() {
         if (newTodo.value.trim()!=='') {
             todos.value.push({ text:newTodo.value })
+            checkTodo.value.push(false)
             newTodo.value = ''
         }
     }
@@ -30,10 +49,17 @@
         todos.value.splice(index, 1);
     }
 
+    function removeChecked(index:number){
+        if (checkTodo.value[index]) {
+            todos.value.splice(index, 1);
+            checkTodo.value.splice(index, 1);
+        }
+    }
+
+
 </script>
 
 <style scoped>
-/* scoped是局部样式，一定要加 */
     .list{
         background-color: skyblue;
         box-shadow: 0 0 10px;
@@ -41,8 +67,5 @@
     }
     button{
         margin:0 10px;
-    }
-    .br{
-        margin:10px 0;
     }
 </style>
